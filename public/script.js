e69d3a659975bdedd297c1efe605672b5b49f98e
François Nsengimana <francknsengimana@gmail.com>
2025-05-02 12:50:44 +0100

    mise a jour des info avec mes propre donnee et non celui du formateur et mise en place du code ts de recuperation des donnee avec les evenement

--- public/script.js
+++ public/script.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("Hello");
+const form = document.getElementById('form');
+const type = document.getElementById('type');
+const firstname = document.getElementById('firstname');
+const lastname = document.getElementById('lastname');
+const address = document.getElementById('address');
+const country = document.getElementById('country');
+const town = document.getElementById('town');
+const zip = document.getElementById('zip');
+const product = document.getElementById('product');
+const price = document.getElementById('price');
+const quantity = document.getElementById('quantity');
+const tva = document.getElementById('tva');
+//submit form
+form.addEventListenerListener("submit", (e) => {
+    e.preventDefault();
+    console.log(type.value, firstname.value);
+});
